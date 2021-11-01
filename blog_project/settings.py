8b4c4d79f154c6b2964121d244c0efa91c22366e
KevonneM <Kevonnem@gmail.com>
2021-11-01 14:21:09 -0600

    settings additions

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-from secret_settings import *
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
